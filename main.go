e76cdf0bfdedcc0ecd2718792830cbee7bc3a278
kYroL01 <michelecampus5@gmail.com>
2025-01-15 17:45:55 +0100

    Add listAvailableDevices options and version 1.0.0

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
+// Copyright 2025 Michele Campus michelecampus5@gmail.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -18,19 +33,40 @@ type Options struct {
 	PcapFile  string
 }
 
+// listAvailableDevices lists all the available network devices
+func listAvailableDevices() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatalf("Error finding devices: %v", err)
+	}
+
+	fmt.Println("Available devices:")
+	for _, device := range devices {
+		fmt.Printf("Name: %s, Description: %s\n", device.Name, device.Description)
+	}
+}
+
+// parseOptions parses the command-line options
 func parseOptions() Options {
 	// Define flags
 	interfaceName := flag.String("interface", "", "Name of the interface to capture packets from")
 	pcapFile := flag.String("pcap", "", "Path to the pcap file to read")
+	listDevices := flag.Bool("list", false, "List available devices")
 
-	// Parse the flags
+	// Parse flags
 	flag.Parse()
 
-	// Check if at least one option is provided
+	// Check if the listDevices flag is provided
+	if *listDevices {
+		listAvailableDevices()
+		os.Exit(0)
+	}
+
+	// Check if at least one option is provided for interface or pcap file
 	if *interfaceName == "" && *pcapFile == "" {
 		fmt.Println("Usage:")
 		flag.PrintDefaults()
-		os.Exit(1)
+		os.Exit(0)
 	}
 
 	return Options{
@@ -40,6 +76,8 @@ func parseOptions() Options {
 }
 
 func main() {
+
+	fmt.Printf(">>--- WELCOME to ngcp_archer %s--->\n", version)
 	// Parse options
 	opts := parseOptions()
 
@@ -50,6 +88,7 @@ func main() {
 	}
 }
 
+// readLiveTraffic reads live traffic from the specified interface
 func readLiveTraffic(interfaceName string) {
 	handle, err := pcap.OpenLive(interfaceName, 1600, true, pcap.BlockForever)
 	if err != nil {
@@ -63,6 +102,7 @@ func readLiveTraffic(interfaceName string) {
 	}
 }
 
+// readPcapFile reads packets from a pcap file
 func readPcapFile(filename string) {
 	handle, err := pcap.OpenOffline(filename)
 	if err != nil {
